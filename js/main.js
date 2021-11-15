466e419c183087e841d15f7f384bd11d52d9f54f
Sergey_Korobejnik <ksn.u@yandex.ua>
2021-11-15 18:29:29 +0200

    final Peretz demo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 let onClickhandler = function (selector, activeClass, activeClasses = [], absolutePos = []) {
     let temp = document.querySelector(`.${selector}`);
 
@@ -31,7 +32,11 @@ function widthChange(event, targetAction) {
     let eventTarget = document.querySelector(`.${event}`).addEventListener('click', selector => {
         selector = document.querySelector(`.${targetAction}`);
         if (selector.style.width == '') {
-            selector.style.width = '21vw';
+            if(window.matchMedia('min-width: 1024px') == true) {
+                selector.style.width = '23vw';
+            } else {
+                selector.style.width = '100vw';
+            }
         } else {
             selector.style.width = '';
         }
@@ -41,8 +46,7 @@ function widthChange(event, targetAction) {
 function mouseOut(selector, activeClass) {
     let temp = document.querySelectorAll(`${selector}`);
     temp.forEach(element => {
-        element.addEventListener('click', function () {
-
+        element.addEventListener('click', function () {            
             element.childNodes.forEach(innerElement => {
                 if (innerElement.nodeName == '#text') return;
                 if (innerElement.classList.contains('dropdown') == true) {
@@ -50,6 +54,14 @@ function mouseOut(selector, activeClass) {
                         maxHeight: innerElement.childNodes[1].offsetHeight,
                         duration: 0.8,
                     });
+                    gsap.fromTo(innerElement.querySelectorAll('.dropdown-holder a'),{
+                        x: '-100%',
+                    },
+                    {
+                        x: '0%',
+                        stagger: 0.1,
+                        duration: 0.8
+                    });                        
                     innerElement.addEventListener('mouseleave', () => {
                         gsap.to(innerElement, {
                             maxHeight:  0,
@@ -76,10 +88,16 @@ onClickhandler('search', 'active', ['search-input', 'close-icon'], ['search-hold
 onClickhandler('close-icon', 'active', ['search-input', 'close-icon'], ['search-holder', 'body-overlay']);
 widthChange('search', 'search-holder');
 widthChange('close-icon', 'search-holder');
-mouseOut('#header .contacts > li, .nav-menu > li');
+mouseOut('.header .contacts > li, .nav-menu > li');
 
 
 function  test(lol) {
-    lol = document.querySelector('#header .contacts .dropdown').childNodes;
+    lol = document.querySelectorAll('#header .contacts > li, .nav-menu > li');
     return lol;
 }
+
+$('.burger').click(function () {
+    $(this).toggleClass('burger-active');
+    $('nav').toggleClass('nav-active');
+    $('body').toggleClass('body-overlay');
+});
